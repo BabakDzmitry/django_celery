dfe03c20eebaf12616764d65847166a15eb7f9be
Dzmitry Babak <alexgott1990@gmail.com>
2020-07-08 23:45:20 +0300

    add:first task by time

--- django_cron/blog/tasks.py
+++ django_cron/blog/tasks.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
+from celery.task import periodic_task
+from celery.schedules import crontab
+
+
+@periodic_task(run_every=(crontab(minute='*/1')), name='FT')
+def my_first_task():
+    print('This is my first task')
 
 
 @shared_task
@@ -16,5 +23,3 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
-
-
